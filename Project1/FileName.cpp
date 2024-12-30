b1742e7389ce4f38046e8828c5b943f82488bfab
JangJinWoo <aktsksqkq@gmail.com>
2024-12-30 16:15:06 +0900

    3주차 첫 번째 필수 구현

---
+++ Project1/FileName.cpp
@@ -0,0 +1,43 @@
+﻿#include "SimpleVector.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	SimpleVector<int>* Tester = new SimpleVector<int>;
+	cout << "Tester의 최대 사이즈 : " << Tester->capacity() << endl;
+
+	Tester->push_back(1);
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	Tester->push_back(2);
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	Tester->push_back(3);
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	Tester->push_back(4);
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	Tester->pop_back();
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	Tester->push_back(4);
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	Tester->pop_back();
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	delete Tester;
+
+	return 0;
+}
---
+++ Project1/SimpleVector.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#include <iostream>
+
+#define DEFAULT_CAPACITY 10
+template <typename T>
+class SimpleVector
+{
+private:
+	T* arr;
+	int currentSize;
+	int currentCapacity;
+
+public:
+	SimpleVector(int _value = DEFAULT_CAPACITY) : currentSize(0)
+	{
+		arr = new T[_value];
+		currentCapacity = _value;
+	}
+
+	~SimpleVector()
+	{
+		delete[] arr;
+	}
+
+	void push_back(const T _value);
+	void pop_back();
+	int size();
+	int capacity();
+};
+
+template<typename T>
+inline void SimpleVector<T>::push_back(const T _inValue)
+{
+	if (currentCapacity > currentSize)
+	{
+		arr[currentSize] = _inValue;
+		currentSize++;
+	}
+}
+
+template<typename T>
+inline void SimpleVector<T>::pop_back()
+{
+	if (currentSize > 0)
+	{
+		currentSize--;
+	}
+}
+
+template<typename T>
+inline int SimpleVector<T>::size()
+{
+	return currentSize;
+}
+
+template<typename T>
+inline int SimpleVector<T>::capacity()
+{
+	return currentCapacity;
+}
+
