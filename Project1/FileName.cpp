3ea0ea89b7c6a8cd5484154230596c663f18197d
JangJinWoo <aktsksqkq@gmail.com>
2024-12-31 20:52:51 +0900

    도전과제

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -6,13 +6,35 @@ using namespace std;
 
 int main()
 {
-	SimpleVector<int>* Tester = new SimpleVector<int>;
+	SimpleVector<int>* Tester = new SimpleVector<int>(3);
 	cout << "Tester의 최대 사이즈 : " << Tester->capacity() << endl;
 
+	//push, pop 테스트
 	Tester->push_back(1);
 
 	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
 
+	Tester->pop_back();
+
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+
+
+
+	//객체복붙 테스트
+	Tester->push_back(1);
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+	SimpleVector<int>* CopyTest = new SimpleVector<int>(*Tester);
+	cout << "CopyTest의 현재 사이즈 : " << Tester->size() << endl;
+
+	delete CopyTest;
+	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+
+
+
+
+	//resize 테스트
 	Tester->push_back(2);
 
 	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
@@ -24,20 +46,7 @@ int main()
 	Tester->push_back(4);
 
 	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
-
-	Tester->pop_back();
-
-	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
-
-	Tester->push_back(4);
-
-	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
-
-	Tester->pop_back();
-
-	cout << "Tester의 현재 사이즈 : " << Tester->size() << endl;
+	cout << "Tester의 최대 사이즈 : " << Tester->capacity() << endl;
 
 	delete Tester;
-
-	return 0;
 }
--- Project1/SimpleVector.h
+++ Project1/SimpleVector.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <iostream>
+#include <algorithm>
 
 #define DEFAULT_CAPACITY 10
 template <typename T>
@@ -7,8 +8,8 @@ class SimpleVector
 {
 private:
 	T* arr;
-	int currentSize;
-	int currentCapacity;
+	int currentSize; //실제 배열에 참
+	int currentCapacity;// 넣을수 있는 배열 최대치
 
 public:
 	SimpleVector(int _value = DEFAULT_CAPACITY) : currentSize(0)
@@ -17,27 +18,45 @@ public:
 		currentCapacity = _value;
 	}
 
+	//생성자 : 다른 객체 복붙(ver.deep)
+	SimpleVector(const SimpleVector& _obj)
+	{
+		this->arr = new T[_obj.currentCapacity];
+		this->currentSize = _obj.currentSize;
+		this->currentCapacity = _obj.currentCapacity;
+		for (int i = 0; i < _obj.currentSize; i++)
+		{
+			this->arr[i] = _obj.arr[i];
+		}
+	}
+
+	//소멸자
 	~SimpleVector()
 	{
 		delete[] arr;
 	}
 
-	void push_back(const T _value);
-	void pop_back();
-	int size();
-	int capacity();
+	void push_back(const T _value);//배열 끝에 값넣기, 가득차면 사이즈 넣기
+	void pop_back();//배열 끝부분 삭제
+	int size();// 현재 배열이 얼마나 차있는지
+	int capacity();//배열 최대치값
+	void sordData();// 배열 정렬, sort함수 사용
+	void resize(int );//배열의 크기를 늘리고, 복붙(deep) 후 기존 것은 delete
 };
 
 template<typename T>
 inline void SimpleVector<T>::push_back(const T _inValue)
 {
-	if (currentCapacity > currentSize)
+	if (currentSize >= currentCapacity) // 가득참
 	{
-		arr[currentSize] = _inValue;
-		currentSize++;
+		resize(currentCapacity + 5);
 	}
+	
+	arr[currentSize] = _inValue;
+	currentSize++;
 }
 
+//우려사항 1: 동적할당된 데이터를 넣었으면? 스마터 포인터로하면 나중에 값바꿀때 상관 없나?
 template<typename T>
 inline void SimpleVector<T>::pop_back()
 {
@@ -58,4 +77,31 @@ inline int SimpleVector<T>::capacity()
 {
 	return currentCapacity;
 }
+
+template<typename T>
+inline void SimpleVector<T>::sordData()
+{
+	sort(arr, arr + currentSize);
+}
+
+template<typename T>
+inline void SimpleVector<T>::resize(int _addCap)
+{
+	if (_addCap <= currentCapacity)
+	{
+		return;
+	}
+
+	T* tmpPtr = new T[_addCap];
+
+	for (int i = 0; i < currentCapacity; i++)
+	{
+		tmpPtr[i] = arr[i];
+	}
+	delete[] arr;
+
+	arr = tmpPtr;
+	currentCapacity = _addCap;
+}
+
 
